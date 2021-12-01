674b224a44bcdc97ffda76281bfe9d24045b428b
rmsharp <rmsharp@me.com>
2021-12-01 12:01:11 -0600

    removed extra space in example lines

--- R/countKinshipValues.R
+++ R/countKinshipValues.R
@@ -5,48 +5,48 @@
 #' (kinship values), and \code{kCounts} (kinship counts).
 #' @examples
 #' \donttest{
-#'  ped <- nprcgenekeepr::smallPed
-#'  simParent_1 <- list(id = "A",
-#'                      sires = c("s1_1", "s1_2", "s1_3"),
-#'                      dams = c("d1_1", "d1_2", "d1_3", "d1_4"))
-#'  simParent_2 <- list(id = "B",
-#'                      sires = c("s1_1", "s1_2", "s1_3"),
-#'                      dams = c("d1_1", "d1_2", "d1_3", "d1_4"))
-#'  simParent_3 <- list(id = "E",
-#'                      sires = c("A", "C", "s1_1"),
-#'                      dams = c("d3_1", "B"))
-#'  simParent_4 <- list(id = "J",
-#'                      sires = c("A", "C", "s1_1"),
-#'                      dams = c("d3_1", "B"))
-#'  simParent_5 <- list(id = "K",
-#'                      sires = c("A", "C", "s1_1"),
-#'                      dams = c("d3_1", "B"))
-#'  simParent_6 <- list(id = "N",
-#'                      sires = c("A", "C", "s1_1"),
-#'                      dams = c("d3_1", "B"))
-#'  allSimParents <- list(simParent_1, simParent_2, simParent_3,
-#'                        simParent_4, simParent_5, simParent_6)
+#' ped <- nprcgenekeepr::smallPed
+#' simParent_1 <- list(id = "A",
+#'                     sires = c("s1_1", "s1_2", "s1_3"),
+#'                     dams = c("d1_1", "d1_2", "d1_3", "d1_4"))
+#' simParent_2 <- list(id = "B",
+#'                     sires = c("s1_1", "s1_2", "s1_3"),
+#'                     dams = c("d1_1", "d1_2", "d1_3", "d1_4"))
+#' simParent_3 <- list(id = "E",
+#'                     sires = c("A", "C", "s1_1"),
+#'                     dams = c("d3_1", "B"))
+#' simParent_4 <- list(id = "J",
+#'                     sires = c("A", "C", "s1_1"),
+#'                     dams = c("d3_1", "B"))
+#' simParent_5 <- list(id = "K",
+#'                     sires = c("A", "C", "s1_1"),
+#'                     dams = c("d3_1", "B"))
+#' simParent_6 <- list(id = "N",
+#'                     sires = c("A", "C", "s1_1"),
+#'                     dams = c("d3_1", "B"))
+#' allSimParents <- list(simParent_1, simParent_2, simParent_3,
+#'                       simParent_4, simParent_5, simParent_6)
 #'
-#'  extractKinship <- function(simKinships, id1, id2, simulation) {
-#'    ids <- dimnames(simKinships[[simulation]])[[1]]
-#'    simKinships[[simulation]][seq_along(ids)[ids == id1],
-#'                              seq_along(ids)[ids == id2]]
-#'  }
+#' extractKinship <- function(simKinships, id1, id2, simulation) {
+#'   ids <- dimnames(simKinships[[simulation]])[[1]]
+#'   simKinships[[simulation]][seq_along(ids)[ids == id1],
+#'                             seq_along(ids)[ids == id2]]
+#' }
 #'
-#'  extractKValue <- function(kValue, id1, id2, simulation) {
-#'    kValue[kValue$id_1 ==  id1 & kValue$id_2 == id2, paste0("sim_", simulation)]
-#'  }
+#' extractKValue <- function(kValue, id1, id2, simulation) {
+#'   kValue[kValue$id_1 ==  id1 & kValue$id_2 == id2, paste0("sim_", simulation)]
+#' }
 #'
-#'  n <- 10
-#'  simKinships <- createSimKinships(ped, allSimParents, pop = ped$id, n = n)
-#'  kValues <- kinshipMatricesToKValues(simKinships)
-#'  extractKValue(kValues, id1 = "A", id2 = "F", simulation = 1:n)
-#'  counts <- countKinshipValues(kValues)
-#'  n <- 10
-#'  simKinships <- createSimKinships(ped, allSimParents, pop = ped$id, n = n)
-#'  kValues <- kinshipMatricesToKValues(simKinships)
-#'  extractKValue(kValues, id1 = "A", id2 = "F", simulation = 1:n)
-#'  cummulatedCounts <- countKinshipValues(kValues, counts)
+#' n <- 10
+#' simKinships <- createSimKinships(ped, allSimParents, pop = ped$id, n = n)
+#' kValues <- kinshipMatricesToKValues(simKinships)
+#' extractKValue(kValues, id1 = "A", id2 = "F", simulation = 1:n)
+#' counts <- countKinshipValues(kValues)
+#' n <- 10
+#' simKinships <- createSimKinships(ped, allSimParents, pop = ped$id, n = n)
+#' kValues <- kinshipMatricesToKValues(simKinships)
+#' extractKValue(kValues, id1 = "A", id2 = "F", simulation = 1:n)
+#' cummulatedCounts <- countKinshipValues(kValues, counts)
 #' }
 #'
 #' @param kinshipValues matrix of kinship values from simulated pedigrees where each
