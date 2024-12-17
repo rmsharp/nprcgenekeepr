bb9aa24040ecd3610cf648ea8d95d846a7196801
R. Mark Sharp <rmsharp@me.com>
2024-12-17 17:08:54 +0900

    abstracted out removal of auto generated IDs

---
+++ R/remove_auto_gen_ids.R
@@ -0,0 +1,25 @@
+#' Remove automatically generated IDs from pedigree
+#'
+#' Currently uses leading "U" to identify automatically generated IDs.
+#' TODO change identification of automatically generated IDs from looking for
+#' an initial "U" at the beginning of an ID to a function call so that actual
+#' ID that start with a "U" are possible.
+#' @param ped datatable that is the `Pedigree`. It contains pedigree
+#' information. The \code{id}, \code{sire}, and \code{dame} columns are
+#' required.
+#'
+#' @return A pedigree with automatically generated IDs removed.
+#' @export
+#'
+#' @examples
+#' examplePedigree <- nprcgenekeepr::examplePedigree
+#' length(ped$id)
+#' ped <- remove_auto_gen_ids(examplePedigree)
+#' length(ped$id)
+#'
+remove_auto_gen_ids <- function(ped) {
+  ped <- ped[!stri_sub(ped$id, 1, 1)  == "U", ]
+  ped$sire[stri_sub(ped$sire, 1, 1)  == "U"] <- NA
+  ped$dam[stri_sub(ped$dam, 1, 1)  == "U"] <- NA
+  ped
+}
