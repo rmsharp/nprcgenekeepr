a7a3a49621a540013b634a8e3fdfc740472bbbde
R. Mark Sharp <rmsharp@me.com>
2024-12-19 17:10:09 +0900

    added lifecycle badges; added hexlogo image file

---
+++ inst/extdata/create_nprcgenekeepr_hexbadge.R
@@ -0,0 +1,4 @@
+library(hexSticker)
+imgurl <- system.file("help/figures/card.png", package="nprcgenekeepr")
+sticker(imgurl, package="nprcgenekeepr", p_size=17, s_x=1, s_y=.75, s_width=.5,
+        filename="man/figures/imgfile.png")
